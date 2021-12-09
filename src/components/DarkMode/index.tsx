795f83291b65c3f4b4e6fbd2bf23009203fd899c
jmau111 <6878027+jmau111@users.noreply.github.com>
2021-12-09 20:31:43 +0100

    delete useless code and don't use implicit type any

--- src/components/DarkMode/index.tsx
+++ src/components/DarkMode/index.tsx
@@ -3,12 +3,14 @@ import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 type DarkModeProps = {
     children: React.ReactNode,
+    theme: any,
+    toggleTheme: any,
 }
 
 const DarkMode: React.FunctionComponent<DarkModeProps> = () => {
     return (
         <ThemeToggler>
-            {({ theme, toggleTheme }) => (
+            {({ theme, toggleTheme }: DarkModeProps) => (
                 <label className="toggle">
                     <input
                         aria-label="Toggle dark mode"
@@ -16,10 +18,10 @@ const DarkMode: React.FunctionComponent<DarkModeProps> = () => {
                         onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
                         checked={theme === 'dark'}
                     />
-                    { theme === "dark" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    {theme === "dark" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                    </svg> }
-                    { theme === "light" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    </svg>}
+                    {theme === "light" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <circle cx="12" cy="12" r="5"></circle>
                         <line x1="12" y1="1" x2="12" y2="3"></line>
                         <line x1="12" y1="21" x2="12" y2="23"></line>
