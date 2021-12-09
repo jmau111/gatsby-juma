b22acb3772170cf0bb59a3babb5d2e847f7de4f6
jmau111 <6878027+jmau111@users.noreply.github.com>
2021-12-09 20:34:18 +0100

    fix error property svg

--- src/components/DarkMode/index.tsx
+++ src/components/DarkMode/index.tsx
@@ -18,10 +18,10 @@ const DarkMode: React.FunctionComponent<DarkModeProps> = () => {
                         onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
                         checked={theme === 'dark'}
                     />
-                    {theme === "dark" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    {theme === "dark" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
                     </svg>}
-                    {theme === "light" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    {theme === "light" && <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <circle cx="12" cy="12" r="5"></circle>
                         <line x1="12" y1="1" x2="12" y2="3"></line>
                         <line x1="12" y1="21" x2="12" y2="23"></line>
