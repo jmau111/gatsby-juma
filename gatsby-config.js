5e2d70af77dc473f3b7c751bad4d36757c8f86f8
jmau111 <6878027+jmau111@users.noreply.github.com>
2021-12-17 19:37:01 +0100

    introduce webp config and default opts for plugin sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,21 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-no-sourcemaps`,
