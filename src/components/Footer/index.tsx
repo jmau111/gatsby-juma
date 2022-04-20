89c715c8cd4a112d90e66d2a1b64daf9026bded5
Julien Maury <6878027+jmau111@users.noreply.github.com>
2022-04-20 03:32:09 -0700

    downgrade react dependencies + gatsby react helmet

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,10 @@ type FooterProps = {
 const Footer: React.FunctionComponent<FooterProps> = ({ children, ...props }) => {
   return (
     <footer className="footer" {...props}>
-      <span>Copyright &copy; {new Date().getFullYear()}</span>
+      <span>
+        Copyright &copy;{` `}
+        {new Date().getFullYear()}
+      </span>
       <a href="https://github.com/jmau111/gatsby-juma">Gatsby Juma</a>
       {children}
     </footer>
